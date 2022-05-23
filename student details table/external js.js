ead12b18fe731c12c30a966fdbc8196b795661b0
BiswasAmitav <biswasamit2648@gmail.com>
2022-05-23 11:26:10 +0530

    all practice file has uploaded

---
+++ student details table/external js.js
@@ -0,0 +1,179 @@
+/*var input=prompt("Enter your character");
+
+if(input<0||input<0)
+document.write("This is a Number.");
+
+//else if(input<=a&&input>=z)
+//document.write("Theis is Lower Case Charecter.");
+
+else
+document.write("This is special charecter");
+
+
+*/
+
+/*
+var x=myFunction(4,3);
+
+function myFunction(a,b){
+    return a*b;
+}
+
+console.log(x);*/
+
+
+
+//********************Program for switch case...**************************
+
+/*var digit=prompt("Enter Your Number: ");
+
+switch(digit){
+    case "0":
+        document.write("Zero");
+        break;
+    case "1":
+        document.write("One");
+        break;
+    case "2":
+        document.write("Two");
+        break;
+    case "3":
+        document.write("Three");
+        break;
+    case "4":
+        document.write("Four");
+        break;
+    case "5":
+        document.write("Five");
+        break;
+    case "6":
+        document.write("Six");
+        break;
+    case "7":
+        document.write("Seven");
+        break;
+    case "8":
+        document.write("Eight");
+        break;
+    case "9":
+        document.write("Nine");
+        break;
+    default:
+        document.write("Not a valid digit!");
+    
+}
+
+*/
+
+/*
+var letter=prompt("Enter letter: ");
+
+letter=letter.toLowerCase();
+
+switch(letter){
+    case "a":
+        document.write("Vouwel");
+        break;
+    
+    case "e":
+        document.write("Vouwel");
+        break;
+    case "i":
+        document.write("Vouwel");
+        break;
+    case "o": 
+        document.write("Vouwel");
+        break;
+    case "u":
+        document.write("Vouwel");
+        break;
+    
+    default:
+        document.write("Consonnt");
+}
+
+*/
+
+  /*var i=1;
+
+  while(i<=15){
+      document.write(" "+ i);
+      i=i+1;
+  }
+
+  */
+
+/*
+var i=1;
+var sum=0;
+ while(i<=15){
+   sum=sum+i;
+    i=i+1;
+ }
+document.write(sum<br>);
+*/
+
+
+
+/* ***********************PROGRAM FOR FONDING DIVIDABLE BY 3&5****************************************
+
+
+
+
+document.write("*************************************" + "<br>");
+document.write("List of your no: which are divide by 3 & 5 into : " + num  +"<br>");
+document.write("*************************************"+"<br>")
+document.write("<br>")
+var i=1;
+var sum=0;
+
+while(i<=num){
+    if(i%3==0 && i%5==0){
+        
+        document.write(i + "<br>");
+        sum=sum+i;
+    }
+    i=i+1;
+}
+document.write("<br>");
+document.write("<br>");
+document.write("=============="+"<br>");
+document.write("  "+"Sum is : "+sum+"<br>");
+document.write("=============="+"<br>");
+document.write("<h1>End</h1>");
+
+
+*/
+
+/*Math.sqrt(25);
+Math.abs(-5);
+var a="amit";
+console.log(a);
+*/
+
+/*var num1=parseInt(prompt("Enter your first number: "));
+var num2=parseInt(prompt("Enter your second number: "));
+var num3=parseInt(prompt("Enter your third number: "));
+var maximum=Math.max(num1,num2,num3);
+document.write(maximum);*/
+
+/*
+
+var num=parseInt(prompt("How many numbers you have ?"));
+var number=new Array();
+
+for(var i=0;i<num;i++){
+    number[i]=parseInt(prompt("Enter your number "));
+    
+}
+document.write(" "+ number);
+var maximum=Math.max(number[i]);
+document.write(maximum);
+
+*/
+
+
+
+
+
+
